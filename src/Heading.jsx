e2c51786462cfed42798c35bcc1492a96e2e3b68
iemrecetin <99025225+iemrecetin@users.noreply.github.com>
2022-02-08 11:01:15 +0300

    codefrom course

---
+++ src/Heading.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Heading() {
+  const date = new Date(/*2022, 1, 1, 19*/);
+  const currentTime = date.getHours();
+
+  let greeting;
+
+  let customStyle = {
+    color: ""
+  };
+
+  console.log(currentTime);
+
+  if (currentTime < 12) {
+    greeting = "Good Morning";
+    customStyle.color = "red";
+  } else if (currentTime < 18) {
+    greeting = "Good Afternoon";
+    customStyle.color = "green";
+  } else {
+    greeting = "Good Night";
+    customStyle.color = "blue";
+  }
+  return (
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
+  );
+}
+
+export default Heading;
